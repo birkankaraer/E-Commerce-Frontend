9eba76bad555ecec9b222e4a1b4e2e06e2136508
Birkankaraer <karaermustafabirkan@gmail.com>
2023-11-04 17:06:16 +0300

    Commit 4.11.2023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ecommerceapp';
+  title:string = "ecommerceapp";
+  user:string = "Birkan Karaer"
+
 }

e0acb23b4d053be6097220ef2b576af559a44eef
Birkankaraer <karaermustafabirkan@gmail.com>
2023-11-10 16:40:03 +0300

    cart and notifications added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { VatAddedPipe } from './pipes/vat-added.pipe';
 import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 
 import { ToastrModule } from 'ngx-toastr';
+import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { ToastrModule } from 'ngx-toastr';
     NaviComponent,
     TodoComponent,
     VatAddedPipe,
-    FilterPipePipe
+    FilterPipePipe,
+    CartSummaryComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -17,7 +18,7 @@ export class ProductComponent implements OnInit {
 
 
   constructor(private ProductService:ProductService, private activatedRoute:ActivatedRoute,
-    private toastrService:ToastrService){}
+    private toastrService:ToastrService, private cartService:CartService){}
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params=>{
@@ -45,9 +46,10 @@ export class ProductComponent implements OnInit {
   }
 
   addToCart(product:Product){
-    this.toastrService.success("Sepete eklendi",product.productName,{
+    this.toastrService.success("Added to cart",product.productName,{
       progressBar:true
     });
+    this.cartService.addToCart(product);
   }
 
 
